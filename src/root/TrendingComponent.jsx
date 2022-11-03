f3e40100816c1c930726a4214a5b64f16e1e96f3
neekesh <nikesthapa20@gmail.com>
2022-11-03 18:28:12 +0545

    Trending component

---
+++ src/root/TrendingComponent.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+class Trending extends React.Component{
+    render(){
+        return(
+            <div className="col-md-3 col-xl-4 ">
+            
+                <div class="input-group mt-4 mb-4">
+                    <div class="form-outline">
+                        <input id="search-input" type="search" class="form-control" />
+                    </div>
+                    <button id="search-button" type="button" class="btn btn-primary">
+                        <FontAwesomeIcon icon="search" />
+                    </button>
+                </div>
+                                
+                <h3>Trends</h3>
+                <div>
+                    <div>
+                        <h6>Title of the poem</h6>
+                    </div>
+                    <div>
+                        John Doe
+                    </div>
+                    <div>
+                        !.4 Readers
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Trending;
